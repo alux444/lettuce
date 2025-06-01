89d307a37c8fb86991dba551e797567a86238095
Alex Liang <126375355+alux444@users.noreply.github.com>
2025-06-01 14:03:38 +1200

    LettuceDatabase headerfile

--- include/LettuceDatabase.h
+++ include/LettuceDatabase.h
@@ -22,27 +22,35 @@ public:
   bool flushAll();
 
   // key values
-  void set(const std::string& key, const std::string& value);
-  bool get(const std::string& key, std::string& value);
+  void set(const std::string &key, const std::string &value);
+  bool get(const std::string &key, std::string &value);
   std::vector<std::string> keys();
-  std::string type(const std::string& key);
-  bool del(const std::string& key);
-  bool expire(const std::string& key, int seconds);
-  bool rename(const std::string& oldKey, const std::string& newKey);
+  std::string type(const std::string &key);
+  bool del(const std::string &key);
+  bool expire(const std::string &key, int seconds);
+  bool rename(const std::string &oldKey, const std::string &newKey);
 
   // list
-  size_t llen(const std::string& key);
-  void lpush(const std::string& key, const std::string& value);
-  void rpush(const std::string& key, const std::string& value);
-  bool lpop(const std::string& key, std::string& value);
-  bool rpop(const std::string& key, std::string& value);
-  int lrem(const std::string& key, int count, const std::string& value);
-  bool lindex(const std::string& key, int index, std::string& value);
-  bool lset(const std::string& key, int index, const std::string& value);
+  size_t llen(const std::string &key);
+  void lpush(const std::string &key, const std::string &value);
+  void rpush(const std::string &key, const std::string &value);
+  bool lpop(const std::string &key, std::string &value);
+  bool rpop(const std::string &key, std::string &value);
+  int lrem(const std::string &key, int count, const std::string &value);
+  bool lindex(const std::string &key, int index, std::string &value);
+  bool lset(const std::string &key, int index, const std::string &value);
 
   // hashes
-  bool hset(const std::string& key, const std::string field, const std::string value);
-  bool hget(const std::string& key, const std::string field, std::string value);
+  bool hset(const std::string &key, const std::string field, const std::string value);
+  bool hget(const std::string &key, const std::string field, std::string value);
+  bool hexists(const std::string &key, const std::string field);
+  bool hdel(const std::string &key, const std::string field);
+  std::unordered_map<std::string, std::string> hgetall(const std::string &key);
+  std::vector<std::string> hkeys(const std::string &key);
+  std::vector<std::string> hvals(const std::string &key);
+  size_t hlen(const std::string &key);
+  bool hmset(const std::string &key, const std::vector<std::pair<std::string, std::string>> pairs);
+
 private:
   std::mutex db_mutex;
   LettuceDatabase() = default;                                  // default constructor
